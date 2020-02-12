be4d2529868f6d6755f99c99623a23695fb92a44
Tanakorn Tampanya <ducth_miw@hotmail.com>
2020-02-12 17:04:12 +0700

    update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { loginController } from './login.controller';
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { UsersModule } from './user/user.module';
 import { FeedbackModule } from './feedback/feedback.module';
+import { BooksModule } from './books/books.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(),UsersModule,FeedbackModule],
+  imports: [TypeOrmModule.forRoot(),UsersModule,FeedbackModule,BooksModule],
   controllers: [AppController,loginController],
   providers: [AppService],
 })
