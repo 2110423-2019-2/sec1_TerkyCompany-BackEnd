bb7117b77070a7ab60e00e2185759c04433413db
Phatcharapon Jumruspun <pokemonlizardon@gmail.com>
2020-02-19 10:39:56 +0700

    Fixed lots of things
    - Remove university from memberT\n- Change the length of speakername in workshop from 80 to 40\n- Change gender type in memberT from string to enum{male, female}\n- Added cascades on reviews and tags entities\n- Change port in main.ts from 3000 to 3001

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cors());
   app.use(bodyParser.json());
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
