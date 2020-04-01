90c002b09d63eac525b495851528bcbf2038d2cd
thanit456 <too_w_o@hotmail.com>
2020-04-02 00:29:29 +0700

    back review ticket to nothing

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -43,91 +43,91 @@ export class BooksController {
     return this.bookServices.findByParticipant(id);
   }
 
-  // @Post('create')
-  // async create(@Body() bookData: BookEntity): Promise<any> {
-	// var id = bookData.workshop;
-	// //console.log(id);
-	// var canBook = await this.workshopsService.canBook(String(id));
-	// //console.log(canBook);
-	// if(canBook)
-	// 	return await this.bookServices.create(bookData);
-  //   else return "Failed to book due to capacity limit";
-  // }
-
   @Post('create')
-  @UseInterceptors(FilesInterceptor('image', 1, {
-    fileFilter: imageFileFilter,
-    storage: diskStorage({
-      destination: './uploads/ticket_picture',
-      filename: editFileName
-    })
-  }))
-  async create(@Body() Request, @UploadedFiles() file): Promise<any> {
-  
-    var bookData = JSON.parse(Request['request']);
-    
+  async create(@Body() bookData: BookEntity): Promise<any> {
     var id = bookData.workshop;
     //console.log(id);
     var canBook = await this.workshopsService.canBook(String(id));
     //console.log(canBook);
     if(canBook)
       return await this.bookServices.create(bookData);
-    else return "Failed to book due to capacity limit";
+      else return "Failed to book due to capacity limit";
   }
 
+  // @Post('create')
+  // @UseInterceptors(FilesInterceptor('image', 1, {
+  //   fileFilter: imageFileFilter,
+  //   storage: diskStorage({
+  //     destination: './uploads/ticket_picture',
+  //     filename: editFileName
+  //   })
+  // }))
+  // async create(@Body() Request, @UploadedFiles() file): Promise<any> {
+  
+  //   var bookData = JSON.parse(Request['request']);
+    
+  //   var id = bookData.workshop;
+  //   //console.log(id);
+  //   var canBook = await this.workshopsService.canBook(String(id));
+  //   //console.log(canBook);
+  //   if(canBook)
+  //     return await this.bookServices.create(bookData);
+  //   else return "Failed to book due to capacity limit";
+  // }
 
-  @Put(':id/:username/update')
+
+  @Put(':workshop_id/:username/update')
   async update(
-    @Param('id') id,
+    @Param('workshop_id') workshop_id,
     @Param('username') username,
     @Body() bookData: BookEntity,
   ): Promise<any> {
-    bookData.workshop = id;
+    bookData.workshop = workshop_id;
     bookData.memberT = username;
     console.log('Update #' + bookData.workshop);
     console.log('Update #' + bookData.memberT);
     return this.bookServices.update(bookData);
   }
 
-  @Post(':id/:username/ticket')
-  @UseInterceptors(FilesInterceptor('image', 1, {
-    fileFilter: imageFileFilter
-  }))
-  async setTicket(
-    @Param('id') workshopID,
-    @Param('username') username,
-    @UploadedFiles() file
-  ){
-    return this.bookServices.setTicket(workshopID, username, file[0].filename);
-  }
+  // @Post(':id/:username/ticket')
+  // @UseInterceptors(FilesInterceptor('image', 1, {
+  //   fileFilter: imageFileFilter
+  // }))
+  // async setTicket(
+  //   @Param('id') workshopID,
+  //   @Param('username') username,
+  //   @UploadedFiles() file
+  // ){
+  //   return this.bookServices.setTicket(workshopID, username, file[0].filename);
+  // }
 
-  @Get(':id/:username/ticket')
-  async getProfile(@Param('workshop') workshopID, @Param('username') username, @Res() res) {
-    var bookData = await (this.bookServices.findOne(workshopID, username));
+  @Get(':workshop_id/:username/ticket')
+  async getProfile(@Param('workshop_id') workshop_id, @Param('username') username, @Res() res) {
+    var bookData = await (this.bookServices.findOne(workshop_id, username));
     return res.sendFile(bookData['ticketURL'], { root: './uploads/ticket_picture'});
   }
 
   // ? For refunding
-  @Delete(':id/:username/delete')
-  async delete(@Param('id') id, @Param('username') username): Promise<any> {
-    return this.bookServices.delete(id, username);
+  @Delete(':workshop_id/:username/delete')
+  async delete(@Param('workshop_id') workshop_id, @Param('username') username): Promise<any> {
+    return this.bookServices.delete(workshop_id, username);
   }
 }
 
-// -- filter extension
-export const imageFileFilter = (req, file, callback) => {
-  if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
-    return callback(new Error('Only image files are allowed!'), false);
-  }
-  callback(null, true);
-}
+// // -- filter extension
+// export const imageFileFilter = (req, file, callback) => {
+//   if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+//     return callback(new Error('Only image files are allowed!'), false);
+//   }
+//   callback(null, true);
+// }
 
-export const editFileName = (req, file, callback) => {
-  const name = file.originalname.split('.')[0];
-  const fileExtName = extname(file.originalname);
-  const randomName = Array(4)
-    .fill(null)
-    .map(() => Math.round(Math.random() * 16).toString(16))
-    .join('');
-  callback(null, `${name}-${randomName}${fileExtName}`);
-};
+// export const editFileName = (req, file, callback) => {
+//   const name = file.originalname.split('.')[0];
+//   const fileExtName = extname(file.originalname);
+//   const randomName = Array(4)
+//     .fill(null)
+//     .map(() => Math.round(Math.random() * 16).toString(16))
+//     .join('');
+//   callback(null, `${name}-${randomName}${fileExtName}`);
+// };
