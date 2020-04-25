5c894bfe168cd1d88b327d604988c674f4b19aa8
thanit456 <too_w_o@hotmail.com>
2020-04-25 22:14:19 +0700

    fix get image

--- src/workshops/workshops.controller.ts
+++ src/workshops/workshops.controller.ts
@@ -126,27 +126,28 @@ export class WorkshopsController {
   }
 
   @Get(':workshopID/picture')
-  async getProfile(@Param('workshopID') workshopID, @Res() res) {
+  getProfile(@Param('workshopID') workshopID, @Res() res) {
     var workshopData = await (this.workshopsServices.findByID(workshopID));
-    console.log(workshopData)
-    return res.sendFile(workshopData['pictureURL'], { root: './uploads/workshop_picture'});
+    if (workshopData['pictureURL'] == '')
+      return 'Failed to get the image' 
+    return workshopData['pictureURL'];
   }
 }
 
 // -- filter extension
-export const imageFileFilter = (req, file, callback) => {
-  if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
-    return callback(new Error('Only image files are allowed!'), false);
-  }
-  callback(null, true);
-}
+// export const imageFileFilter = (req, file, callback) => {
+//   if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+//     return callback(new Error('Only image files are allowed!'), false);
+//   }
+//   callback(null, true);
+// }
 
-export const editFileName = (req, file, callback) => {
-  const name = file.originalname.split('.')[0];
-  const fileExtName = extname(file.originalname);
-  const randomName = Array(4)
-    .fill(null)
-    .map(() => Math.round(Math.random() * 16).toString(16))
-    .join('');
-  callback(null, `${name}-${randomName}${fileExtName}`);
+// export const editFileName = (req, file, callback) => {
+//   const name = file.originalname.split('.')[0];
+//   const fileExtName = extname(file.originalname);
+//   const randomName = Array(4)
+//     .fill(null)
+//     .map(() => Math.round(Math.random() * 16).toString(16))
+//     .join('');
+//   callback(null, `${name}-${randomName}${fileExtName}`);
 };
